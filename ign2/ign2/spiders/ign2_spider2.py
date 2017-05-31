5c07e804f17590bdcffab028718812db41623f21
iavallejos <iavallejos93@gmail.com>
2017-05-31 00:06:54 -0400

    V2 araña2
    Se arregla el problema de datos faltantes en algunas páginas
    Se mueven los resultados del crawling de 3 páginas a una carpeta resultados en formatos JSON y CSV, también se agrega otro JSON trabajado para que se pueda leer fácilmente
    Se eliminan resultados anteriores

--- ign2/ign2/spiders/ign2_spider2.py
+++ ign2/ign2/spiders/ign2_spider2.py
@@ -4,13 +4,7 @@ import scrapy
 class IGNItem(scrapy.Item):
     title = scrapy.Field()
     description = scrapy.Field()
-
-    genres = scrapy.Field()
-    publisher = scrapy.Field()
-    developers = scrapy.Field()
     platforms = scrapy.Field()
-    price = scrapy.Field()
-    release_date = scrapy.Field()
 
     ign_score = scrapy.Field()
     ign_score_phrase = scrapy.Field()
@@ -18,14 +12,25 @@ class IGNItem(scrapy.Item):
     community_score_phrase = scrapy.Field()
     review_link = scrapy.Field()
 
+    release_date = scrapy.Field()
+    price = scrapy.Field()
+    genres = scrapy.Field()
+    publisher = scrapy.Field()
+    developers = scrapy.Field()
+    rating_category = scrapy.Field()
+    rating_content = scrapy.Field()
+
 
 class IGNSpider2(scrapy.Spider):
     name = "ign2_spider2"
     start_urls = [
-        'http://www.ign.com/games/civilization-beyond-earth/pc-20016115'
+        'http://www.ign.com/games/civilization-beyond-earth/pc-20016115',
+        'http://www.ign.com/games/total-war-battles-shogun/pc-142564',
+        'http://www.ign.com/games/funky-smugglers/iphone-145106'
     ]
 
     def parse(self, response):
+        print("Existing settings: %s" % self.settings.attributes.keys())
 
         item = IGNItem()
 
@@ -33,25 +38,14 @@ class IGNSpider2(scrapy.Spider):
 
         item['description'] = ''
         first = True
-        for text in response.css('div.gameInfo p::text').extract()[1::]:
-            if first:
-                item['description'] += text.strip()
+        for text in response.css('div.gameInfo p::text').extract():
+            if text.strip() == '':
+                item['description'] += text.strip().replace(u"\u2019", "'")
+            elif first:
+                item['description'] += text.strip().replace(u"\u2019", "'")
                 first = False
             else:
-                item['description'] += '\n' + text.strip()
-
-        item['genres'] = ''
-        first = True
-        for genre in response.css('div.gameInfo-list:nth-child(4) > div:nth-child(1) > a::text').extract():
-            if first:
-                item['genres'] += genre.strip()
-                first = False
-            else:
-                item['genres'] += ' ' + genre.strip()
-
-        item['publisher'] = response.css('div.gameInfo-list:nth-child(4) > div:nth-child(2)::text').extract_first().replace(':\n','').strip()
-
-        item['developers'] = response.css('div.gameInfo-list:nth-child(4) > div:nth-child(3)::text').extract_first().replace(':\n','').strip()
+                item['description'] += '\n\n' + text.strip().replace(u"\u2019", "'")
 
         item['platforms'] = ''
         first = True
@@ -62,18 +56,64 @@ class IGNSpider2(scrapy.Spider):
             else:
                 item['platforms'] += ' ' + platform.strip()
 
-        item['price'] = response.css('div.gameInfo-list:nth-child(3) > div:nth-child(2)::text').extract_first().replace(':','').strip()
+        item['ign_score'] = response.css('div.ignRating div.ratingValue::text').extract_first().replace('\n',
+                                                                                                        '').strip()
 
-        item['release_date'] = response.css('div.gameInfo-list:nth-child(3) > div:nth-child(1)::text')[1].extract().replace(':','').strip()
+        item['ign_score_phrase'] = response.css('div.ignRating div.ratingText::text').extract_first(
+            ).replace('\n', '').strip()
 
-        item['ign_score'] = response.css('div.ignRating div.ratingValue::text').extract_first().replace('\n','').strip()
+        item['community_score'] = response.css('div.communityRating div.ratingValue::text').extract_first().replace(
+            '\n', '').strip()
 
-        item['ign_score_phrase'] = response.css('div.ignRating div.ratingText::text').extract_first().replace('\n','').strip()
-
-        item['community_score'] = response.css('div.communityRating div.ratingValue::text').extract_first().replace('\n','').strip()
-
-        item['community_score_phrase'] = response.css('div.communityRating div.ratingText::text').extract_first().replace('\n','').strip()
+        item['community_score_phrase'] = response.css(
+            'div.communityRating div.ratingText::text').extract_first().replace('\n', '').strip()
 
         item['review_link'] = response.css('div.ignRating a.reviewLink::attr(href)').extract_first().strip()
+
+        # Para obtener los valores de la tabla del div "gameInfo-list"
+        megalist = []
+        all_divs = response.css('div.gameInfo div.gameInfo-list')
+        for column in all_divs:  # divs0
+            divs = column.css('div')
+            for div in divs[1::]:
+                first = True
+                seccond = True
+                val = ''
+                for data in div.css('*::text').extract():
+                    temp = data.replace(':', '').strip()
+                    if temp != '':
+                        if first:
+                            megalist.append(temp)
+                            first = False
+                        else:
+                            if seccond:
+                                val += temp
+                                seccond = False
+                            else:
+                                val = val + ", " + temp
+                megalist.append(val)
+
+        for i in range(0, len(megalist), 2):
+            var = megalist[i]  # variable, excepto en caso rate
+            value = megalist[i + 1]  # valor, excepto caso rate
+
+            if var == 'Release Date':
+                item['release_date'] = value
+
+            elif var == 'MSRP':
+                item['price'] = value
+
+            elif var == 'Genre':
+                item['genres'] = value
+
+            elif var == 'Publisher':
+                item['publisher'] = value
+
+            elif var == 'Developer':
+                item['developers'] = value
+
+            else:
+                item['rating_category'] = var
+                item['rating_content'] = value
 
         yield item
