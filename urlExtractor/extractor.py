7503f4e77603402739e3b08e7cb3be54f5cbfc47
Iavallejos <iavallejos93@gmail.com>
2017-06-08 23:32:49 -0400

    Mejorado extractor para que divida la lista en n archivos lo más iguales en largo posible,

--- urlExtractor/extractor.py
+++ urlExtractor/extractor.py
@@ -1,4 +1,9 @@
 import csv
+import os
+
+path = os.path.dirname(os.path.realpath(__file__)) + "/chunks"
+dataBase_path = "ign.csv"
+
 
 def csv_reader(file_obj, debug = 0):
     '''Used to read a csv file and extract its 3rd column to a list, use with debug=1 so it prints what is added '''
@@ -17,7 +22,25 @@ def csv_reader(file_obj, debug = 0):
     print "Finished\nReaded " + str(cont) + " Rows"
     return urlList
 
+
+def chunkanator(data, files):
+    k, m = divmod(len(data), files)
+    cont = 1
+
+    for i in xrange(files):
+        print "Creando archivo " + str(cont)
+        with open(path + '/parte'+str(cont)+'.txt', 'w') as newFile:
+            for link in data[i * k + min(i, m):(i+1) * k + min(i + 1, m)]:
+                newFile.write(link)
+        cont += 1
+
+
 if __name__ == '__main__':
-    file_path = "ign.csv"
-    with open(file_path, "rb") as dataBase:
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+    with open(dataBase_path, "rb") as dataBase:
         list = csv_reader(dataBase)
+
+    print(len(list))
+    chunkanator(list, 9)
