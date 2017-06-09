aa9de46206830d86a29d1e47f07dcc3af85d25b1
Pablo <pablo.badilla@ug.uchile.cl>
2017-06-09 13:12:33 -0400

    Crawler Comentado

--- ign2/ign2/spiders/gameCrawler.py
+++ ign2/ign2/spiders/gameCrawler.py
@@ -1,18 +1,27 @@
-import scrapy
 import os
+import scrapy
 
 
 class IGNItem(scrapy.Item):
+    """IGNItem, objeto generado a partir de los items de scrapy.
+
+    Este objeto contiene los campos extraibles de las páginas IGN.
+    Por lo mismo, en estos se guardará la información extraida desde las
+    páginas de la descripción de los juegos,
+    """
+    # Campos Generales : Titulo, descripción y Plataformas.
     title = scrapy.Field()
     description = scrapy.Field()
     platforms = scrapy.Field()
 
+    # Scores
     ign_score = scrapy.Field()
     ign_score_phrase = scrapy.Field()
     community_score = scrapy.Field()
     community_score_phrase = scrapy.Field()
     review_link = scrapy.Field()
 
+    # Datos del desarrollador y del desarollo del juego.
     release_date = scrapy.Field()
     price = scrapy.Field()
     genres = scrapy.Field()
@@ -26,6 +35,18 @@ class IGNSpider(scrapy.Spider):
     name = "gameCrawler"
 
     def __init__(self, part=None, *args, **kwargs):
+        """Función de inicio del crawler.
+
+        Args:
+            part: Segmento de URL al que se le hará el crawling. Estas estan
+            definidas en los archivos ubuicados en
+            '/urlExtractor/chunks/parte%part.txt'.
+
+        Esta función comienza fijando el directorio en donde se ubican las URL,
+        luego abre el archivo definido por part y termina fijando las URL
+        contenidas en ese archivo como start_urls.
+
+        """
         super(IGNSpider, self).__init__(*args, **kwargs)
         original_path = os.getcwd()
         os.chdir("../")
@@ -38,46 +59,74 @@ class IGNSpider(scrapy.Spider):
             self.start_urls = [url.strip() for url in f.readlines()]
 
     def parse(self, response):
+        """Toma el objeto response y extrae sus características.
+
+        Args:
+            self: Referencia al objeto.
+            response: Objeto respuesta de Request de scrapy a una URL de IGN.
+                      Contiene un descriptor de CSS con información.
+        Returns:
+            void.
+
+        La función principal que realiza parse es extraer las características
+        de las paginas de cada juego indicado en el arrchivo de URL leído.
+        Para esto, crea un nuevo IGNItem, comienza extrayendo el titulo del
+        juego, su descripción y plataforma, luego
+        """
+        # Nuevo item IGN.
         item = IGNItem()
 
+        # Titulo, descripción y plataforma:
         item['title'] = response.css('span.fn::text').extract_first().strip()
 
         item['description'] = ''
         first = True
         for text in response.css('div.gameInfo p::text').extract():
-            if text.strip() == '':
+            if text.strip() == '':  # Caso base: No queda mas texto.
                 item['description'] += text.strip().replace(u"\u2019", "'")
-            elif first:
+            elif first:  # Primer Parrafo.
                 item['description'] += text.strip().replace(u"\u2019", "'")
                 first = False
-            else:
-                item['description'] += '\n\n' + text.strip().replace(u"\u2019", "'")
+            else:  # Parrafo Cualquiera. (Agrega salto de linea).
+                item['description'] += '\n\n' +
+                text.strip().replace(u"\u2019", "'")
 
         item['platforms'] = ''
         first = True
-        for platform in response.css('div.contentPlatformsText a::text').extract():
-            if first:
+        for platform in response.css('div.contentPlatformsText a::text')
+        .extract():
+            if first:  # Primera plataforma.
                 item['platforms'] += platform.strip()
                 first = False
-            else:
+            else:  # Plataforma cualquiera, agrego 2 espacios
                 item['platforms'] += ' ' + platform.strip()
 
-        item['ign_score'] = response.css('div.ignRating div.ratingValue::text').extract_first().replace('\n',
-                                                                                                        '').strip()
+        # Scores: ign_score, ign_score_phrase, community_score,
+        # community_score_phrase
+        item['ign_score'] = response.css('div.ignRating div.ratingValue::text')
+        .extract_first().replace('\n', '').strip()
 
-        item['ign_score_phrase'] = response.css('div.ignRating div.ratingText::text').extract_first(
-            ).replace('\n', '').strip()
+        item['ign_score_phrase'] =
+        response.css('div.ignRating div.ratingText::text')
+        .extract_first().replace('\n', '').strip()
 
-        item['community_score'] = response.css('div.communityRating div.ratingValue::text').extract_first().replace(
-            '\n', '').strip()
+        item['community_score'] =
+        response.css('div.communityRating div.ratingValue::text')
+        .extract_first().replace('\n', '').strip()
 
-        item['community_score_phrase'] = response.css(
-            'div.communityRating div.ratingText::text').extract_first().replace('\n', '').strip()
+        item['community_score_phrase'] =
+        response.css('div.communityRating div.ratingText::text')
+        .extract_first().replace('\n', '').strip()
 
-        item['review_link'] = response.css('div.ignRating a.reviewLink::attr(href)').extract_first().strip()
+        # Link del Review.
+        item['review_link'] =
+        response.css(IGNItem'div.ignRating a.reviewLink::attr(href)')
+        .extract_first().strip()
 
-        # Para obtener los valores de la tabla del div "gameInfo-list"
-        megalist = []
+        # Extracción de los valores de la tabla del div "gameInfo-list":
+        # release_date, MSRP, Genre, Publisher, Developer, rating_category.
+
+        megalist = []  # Arreglo en donde se guardarán los datos extraidos.
         all_divs = response.css('div.gameInfo div.gameInfo-list')
         for column in all_divs:
             divs = column.css('div')
@@ -99,9 +148,10 @@ class IGNSpider(scrapy.Spider):
                                 val = val + ", " + temp
                 megalist.append(val)
 
+        # Por cada dato extraido de gameInfo-list.
         for i in range(0, len(megalist), 2):
-            var = megalist[i]  # variable, excepto en caso rate
-            value = megalist[i + 1]  # valor, excepto caso rate
+            var = megalist[i]  # Nombre de la variable, excepto en caso rate.
+            value = megalist[i + 1]  # Valor de la variable, excepto caso rate.
 
             if var == 'Release Date':
                 item['release_date'] = value
